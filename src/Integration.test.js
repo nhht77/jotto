4a75b53c503cf69fb744cd638644619a465a49d9
nhht77 <nhht77@gmail.com>
2019-10-05 21:27:13 +0300

    Tests

---
+++ src/Integration.test.js
@@ -0,0 +1,16 @@
+import { storeFactory } from '../test/testUtils';
+import { guessWord } from './actions/guessWord';
+
+describe('guessWord action dispatcher', () => {
+  describe('There is some guessed words', () => {
+    test('update state correctly for unsuccessful guess', () => {});
+
+    test('update state correctly for successful guess', () => {});
+  });
+
+  describe('There is no guessed words', () => {
+    test('update state correctly for unsuccessful guess', () => {});
+
+    test('update state correctly for successful guess', () => {});
+  });
+});
